1dd18cf6e00c49335d0275bc9eadc643c1d43b2b
Marcos <marcoseduc2019@gmail.com>
2024-08-20 17:36:05 -0300

    mudanças

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import AdminPage from './AdminPage';
-import ManageDocuments from './ManageDocuments'; // Importe a página de gerenciamento de documentos
+import ManageDocuments from './ManageDocuments';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <nav>
+          <ul>
+            <li><Link to="/">Admin Page</Link></li>
+            <li><Link to="/manage-documents">Manage Documents</Link></li>
+          </ul>
+        </nav>
         <Routes>
           <Route path="/" element={<AdminPage />} />
           <Route path="/manage-documents" element={<ManageDocuments />} />
